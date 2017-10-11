bc375106b6fda38c6d31b2fd731a8cabc4b98b2e
nealou <neallausson@gmail.com>
2017-10-11 18:51:11 +0200

    card ,deck and Hand

--- src/Card.java
+++ src/Card.java
@@ -19,9 +19,29 @@ public class Card {
     //Methodes
     public void Action()
     {
-        if (    )
+        if ( race==0) // Korrigan
         {
-
+            //draw 2 random cards in opponent hand
+        }
+        if ( race==1) // Gnome
+        {
+            //draw 2 cards
+        }
+        if ( race==2) // Goblin
+        {
+            // switch hand
+        }
+        if ( race==3) // Elf
+        {
+            // copy and use the power of a card of your kingdom
+        }
+        if ( race==4) // Dyrad
+        {
+            // stole a card in the opponent kingdom and putt it in your kingdom without power
+        }
+        if ( race==5) // Troll
+        {
+            //swap kingdom
         }
     }
 
