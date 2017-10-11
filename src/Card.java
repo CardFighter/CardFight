efdf594d20e902894674fa5f0700482af1922333
nealou <neallausson@gmail.com>
2017-10-11 15:54:45 +0200

    card and deck

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,29 @@
 public class Card {
+
+
+    //Attributs
+    private int race ; //0 a 5
+
+    //Getteur et Setteur
+    public int Race() {
+        return race;
+    }
+
+    //Constructeurs
+    public Card(int race)
+    {
+        super();
+        this.race=race ;
+    }
+
+    //Methodes
+    public void Action()
+    {
+        if (    )
+        {
+
+        }
+    }
+
+
 }
